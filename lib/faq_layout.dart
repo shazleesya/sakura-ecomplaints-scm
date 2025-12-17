4de8d6fcaded1ed6a024aa2b6de46faff1ad12fe
Down-l <atiqahaina0612@gmail.com>
2025-12-18 02:17:59 +0800

    Major: Add FAQ layout placeholder #4

---
+++ lib/faq_layout.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+
+class faqLayout extends StatelessWidget {
+  const faqLayout({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: PlaceholderWidget(),
+      ),
+    );
+  }
+}
+
+class PlaceholderWidget extends StatelessWidget {
+  const PlaceholderWidget({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(24),
+      decoration: BoxDecoration(
+        color: Colors.grey.shade200,
+        borderRadius: BorderRadius.circular(12),
+        border: Border.all(color: Colors.grey.shade400),
+      ),
+      child: const Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Icon(Icons.construction, size: 48, color: Colors.grey),
+          SizedBox(height: 16),
+          Text("FAQ Layout Coming Soon", style: TextStyle(
+            fontSize: 18,
+            fontWeight: FontWeight.w600,
+          ),),
+          SizedBox(height: 8),
+          Text(
+            "This section is under development",
+            style: TextStyle(
+              fontSize: 14,
+              color: Colors.black54,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
