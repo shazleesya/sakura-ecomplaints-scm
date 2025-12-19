d96df532b4e7db13648fc6f798506e73dd7a76c7
Simoyyy <zulfadlibio@gmail.com>
2025-12-19 17:18:19 +0800

    Patch: Update home page header text(#2)

--- lib/student_home.dart
+++ lib/student_home.dart
@@ -85,7 +85,7 @@ class HomePageState extends State<HomePage> {
                 ),
                 const SizedBox(width: 10),
                 Text(
-                  "ASSALAMUALAIKUM STUDENTS, ${widget.matricNo}",
+                  "HELLO WELCOME, ${widget.matricNo}",
                   style: const TextStyle(
                     fontSize: 17,
                     color: Color.fromARGB(255, 11, 11, 11),
